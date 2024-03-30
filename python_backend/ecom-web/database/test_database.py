be87d7c2ac06976a172c182a72c59bea7b2a4138
razapanjwani <razapanjwani2006@gmail.com>
2024-03-31 00:46:37 +0500

    added database

--- python_backend/ecom-web/database/test_database.py
+++ python_backend/ecom-web/database/test_database.py
@@ -1,23 +1,23 @@
-import os 
+import os
 from dotenv import find_dotenv,load_dotenv
 from sqlmodel import create_engine,Session,SQLModel
 
 load_dotenv(find_dotenv())
 
-DATABSE_URL = os.environ["DATABASE_URL"]
-engine = create_engine(DATABSE_URL,echo=True)
+TEST_DATABASE_URL = os.environ["TEST_DATABASE_URL"]
+engine = create_engine(TEST_DATABASE_URL,echo=True)
 
 
-def get_session():
+def get_test_session():
     with Session(engine) as session:
         yield session 
 
 
-def create_all_tables():
+def test_create_all_tables():
     SQLModel.metadata.create_all(engine)
     return "Tables created"
 
 
-def drop_all_tables():
+def test_drop_all_tables():
     SQLModel.metadata.drop_all(engine)
     return "Tables dropped"
